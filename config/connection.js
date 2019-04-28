f0cf52ceba9f6d479edd25cae761381d6e53d66d
Sasha Peters <peteram1@sewanee.edu>
2019-04-28 16:01:22 -0500

    removed pw

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123mumnon123',
+    password: '',//password here
     database: 'burgers_db'
   });
 };
