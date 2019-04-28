46b95da23bcdd18ddf4b52cffae6d6f60ac48097
Sasha Peters <peteram1@sewanee.edu>
2019-04-28 15:45:14 -0500

    heroku db changes added

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-// we placed the connections in this source object
-var source = {
-  // localhost
-  localhost: {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "123mumnon123",
-    database: "burgers_db"
-  },
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123mumnon123',
+    database: 'burgers_db'
+  });
 };
 
-// we use source.[name of connection] to hook into mysql
-var connection = mysql.createConnection(source.localhost);
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
